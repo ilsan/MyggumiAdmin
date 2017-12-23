0f05078239dbbfcea62879904cd9226769f34b27
ljh3259 <ljh3259@naver.com>
2017-12-23 12:23:31 +0900

    test service 추가

---
+++ MyggumiAdmin/src/main/java/com/sp/service/Impl/TestServiceImpl.java
@@ -0,0 +1,20 @@
+package com.sp.service.Impl;
+
+import com.sp.service.TestService;
+
+public class TestServiceImpl implements TestService{
+    @Override
+    public void testMethod() {
+
+    }
+
+
+    public static void test(){
+        TestService testService = () -> {
+
+        };
+
+    }
+
+
+}
---
+++ MyggumiAdmin/src/main/java/com/sp/service/TestService.java
@@ -0,0 +1,7 @@
+package com.sp.service;
+
+@FunctionalInterface
+public interface TestService {
+
+    void testMethod();
+}
