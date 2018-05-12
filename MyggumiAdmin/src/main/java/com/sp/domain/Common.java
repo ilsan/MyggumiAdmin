e1f109a589649aea9734d5ac9987bd5570d24a44
ShipJH <BAE@DESKTOP-BJU929Q>
2018-05-13 00:41:41 +0900

    .

--- MyggumiAdmin/src/main/java/com/sp/domain/Common.java
+++ MyggumiAdmin/src/main/java/com/sp/domain/Common.java
@@ -14,8 +14,15 @@ import lombok.Data;
 
 @Data
 public class Common {
+	/** 등록자 */
 	private String regUser;
+	
+	/** 등록일 */
 	private LocalDateTime regDate;
+	
+	/** 수정자 */
 	private String updUser;
+	
+	/** 수정일 */
 	private LocalDateTime updDate;
 }
